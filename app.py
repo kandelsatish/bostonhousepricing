3bd304501003126e46f75bff0592d370c352b546
satishkandel <satishkandel98@gmail.com>
2024-10-04 10:32:27 -0400

    change in app.py and html file

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
+# importing all necessary liberaries 
 import pickle
 from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np 
 import pandas as pd
-
 
 
 # create a flask app or initialize the flask application
@@ -10,12 +10,14 @@ app=Flask(__name__)
 
 # load your model
 model=pickle.load(open('regression_model.pkl','rb'))
-scaler=pickle.load(open('scaler.pkl','rb'))
-@app.route('/')
+scalar=pickle.load(open('scaling.pkl','rb'))
+
+# define endpoints and the actions 
+@app.route('/',methods=['GET'])
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api',method=['POST'])
+@app.route('/predict_api',methods=['POST'])
 def predict_api():
     data=request.json['data']
     print(data)
@@ -24,6 +26,15 @@ def predict_api():
     output=model.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=model.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+
 
 
 
